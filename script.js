a2c3db397324bd9542dd71907145a5c8b7a1688a
Glitch (glitch-hello-website) <none>
2024-07-15 21:19:44 +0000

    🌷💕 Checkpoint ./index.html:62936102/2764 ./script.js:62936102/77

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let sceneEl = document.querySelector("a-scene")
-var el = sceneEl.querySelector('#nyan');
+var el = sceneEl.querySelector('#player');
 console.log(el)
-document.addEventListener("click", ()=>{
+document.addEventListener("click", (event)=>{
   el.body.applyImpulse(
-  /* impulse */        new CANNON.Vec3(0, 1, -1),
+  /* impulse */        new CANNON.Vec3(0, 25, 0),
   /* world position */ new CANNON.Vec3().copy(el.getAttribute('position'))
 );
 })
