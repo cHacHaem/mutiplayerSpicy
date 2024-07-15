553463f9477660ccf28d72010acaf8af588ff214
Glitch (glitch-hello-website) <none>
2024-07-15 21:31:01 +0000

    💡📟 Checkpoint ./script.js:62936102/6590 ./index.html:62936102/16

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-let sceneEl = document.querySelector("a-scene")
-var el = sceneEl.querySelector('#player');
-console.log(el)
-document.addEventListener("click", (event)=>{
-  el.body.applyImpulse(
-  /* impulse */        new CANNON.Vec3(0, 25, 0),
-  /* world position */ new CANNON.Vec3().copy(el.getAttribute('position'))
-);
-})
+AFRAME.registerComponent('player-controls', {
+  init: function () {
+    this.keys = {};
+    this.forceAmount = 100; // Adjust this value for the desired movement speed
+
+    document.addEventListener("keydown", (event) => {
+      this.keys[event.key] = true;
+    });
+
+    document.addEventListener("keyup", (event) => {
+      this.keys[event.key] = false;
+    });
+  },
+
+  tick: function (time, timeDelta) {
+    const el = this.el;
+    const force = new CANNON.Vec3();
+    const forceAmount = this.forceAmount;
+
+    if (this.keys['w'] || this.keys['ArrowUp']) {
+      force.z -= forceAmount;
+    }
+    if (this.keys['s'] || this.keys['ArrowDown']) {
+      force.z += forceAmount;
+    }
+    if (this.keys['a'] || this.keys['ArrowLeft']) {
+      force.x -= forceAmount;
+    }
+    if (this.keys['d'] || this.keys['ArrowRight']) {
+      force.x += forceAmount;
+    }
+    if (this.keys[' ']) { // Spacebar for jump
+      force.y += forceAmount;
+    }
+
+    if (!force.almostZero()) {
+      el.body.applyForce(force, el.body.position);
+    }
+  }
+});
+
+document.querySelector('#player').setAttribute('player-controls', '');
