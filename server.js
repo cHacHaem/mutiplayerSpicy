9265049d0a3970fb8e69877226349a0eb195de94
Glitch (glitch-hello-website) <none>
2024-07-17 13:17:50 +0000

    💎🌷 Checkpoint ./public/index.html:62936102/223 ./server.js:62936102/3640

--- server.js
+++ server.js
@@ -13,14 +13,8 @@ server.listen(port, function () {
 app.use(express.static('public'));
 
 // Chatroom
-
-var numUsers = 0;
-
 io.on('connection', function (socket) {
-  var addedUser = false;
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
+  socket.on('position update', function (data) {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
@@ -28,48 +22,13 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+  socket.on('disconnect', function () {
+    // echo globally that this client has left
+    socket.broadcast.emit('user left', {
       username: socket.username,
       numUsers: numUsers
     });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+  }); // Added missing closing parenthesis and semicolon
 });
+
+  // when the client emits 'new message', this listens and executes
