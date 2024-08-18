dbdaf6ae609e486e04e2822fc86903131b97f210
Glitch (glitch-hello-website) <none>
2024-08-18 17:37:19 +0000

    🤘🍢 Checkpoint ./public/index.html:64788585/111 ./public/multiplayer.js:64788585/248

--- public/multiplayer.js
+++ public/multiplayer.js
@@ -9,11 +9,13 @@ function sendUpdate() {
 setInterval(sendUpdate, 60)
 socket.on("player update", (stuff) =>{
   if(stuff.id != playerId && !(stuff.id in players)) {
-    let newPlayer = document.createElement("a-entity");
-     newPlayer.setAttribute("move", "jkw")
-    newPlayer.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle.glb?v=1723984484360")
-    newPlayer.setAttribute("scale", "200 200, 200")
-  newPlayer.setAttribute("static-body", "shape", "hull")
+    let newPlayer = document.createElement("a-sphere");
+    let newPlayerSkin = document.createElement("a-entity");
+     newPlayerSkin.setAttribute("move", "jkw")
+    newPlayerSkin.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle.glb?v=1723984484360")
+    newPlayerSkin.setAttribute("scale", "200 200 200")
+  newPlayer.setAttribute("static-body", "shape", "sphere")
+    newPlayer.appendChild(newPlayerSkin)
     players[stuff.id] = newPlayer;
     scene.appendChild(newPlayer);
   } else if(stuff.id in players) {
