616aa1b8fcb8c44094713b6cd7960e3cfef4fa6e
Glitch (glitch-hello-website) <none>
2024-08-18 12:36:42 +0000

    🧀🌞 Checkpoint ./public/multiplayer.js:64788585/340 ./.glitch-assets:64788585/391 ./public/index.html:64788585/189

--- public/multiplayer.js
+++ public/multiplayer.js
@@ -9,9 +9,9 @@ function sendUpdate() {
 setInterval(sendUpdate, 60)
 socket.on("player update", (stuff) =>{
   if(stuff.id != playerId && !(stuff.id in players)) {
-    let newPlayer = document.createElement("a-sphere");
-    newPlayer.setAttribute("color", "green")
-    newPlayer.setAttribute("static-body", "shape", "sphere")
+    let newPlayer = document.createElement("a-entity");
+    newPlayer.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle.glb?v=1723984484360")
+    newPlayer.setAttribute("static-body", "shape", "auto")
     players[stuff.id] = newPlayer;
     scene.appendChild(newPlayer);
   } else if(stuff.id in players) {
