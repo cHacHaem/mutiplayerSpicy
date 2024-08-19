9f777b46007b9122f13acab4173b7ad780a846f3
Glitch (glitch-hello-website) <none>
2024-08-19 18:18:05 +0000

    💶👨‍❤️‍💋‍👨 Checkpoint ./public/multiplayer.js:64788585/319

--- public/multiplayer.js
+++ public/multiplayer.js
@@ -9,18 +9,20 @@ function sendUpdate() {
 setInterval(sendUpdate, 60)
 socket.on("player update", (stuff) =>{
   if(stuff.id != playerId && !(stuff.id in players)) {
-    let newPlayer = document.createElement("a-cylinder");
-    let newPlayerSkin = document.createElement("a-entity");
-     newPlayerSkin.setAttribute("move", "jkw")
-    newPlayerSkin.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle2.glb?v=1724004739221")
-    newPlayerSkin.setAttribute("scale", "0.4 0.4 0.4")
-    newPlayerSkin.setAttribute("position", "")
+    let newPlayerHitbox = document.createElement("a-cylinder");
+    let newPlayer = document.createElement("a-entity");
+     newPlayer.setAttribute("move", "jkw")
+    newPlayer.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle2.glb?v=1724004739221")
+    newPlayer.setAttribute("scale", "0.4 0.4 0.4")
+    newPlayer.setAttribute("position", "0 -2 0")
+    newPlayer.setAttribute("visible", "false")
+    newPlayer.setAttribute("visible", "true")
   newPlayer.setAttribute("static-body", "shape", "cylinder")
+    newPlayerHitbox.setAttribute("height", "4")
     newPlayer.appendChild(newPlayerSkin)
     players[stuff.id] = newPlayer;
     scene.appendChild(newPlayer);
   } else if(stuff.id in players) {
-    stuff.position.y += 1
     players[stuff.id].setAttribute("position", stuff.position);
   }
 })
