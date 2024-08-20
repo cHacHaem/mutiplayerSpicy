6e462ffd6d9637c096ffe4da038b0e3e06706510
Glitch (glitch-hello-website) <none>
2024-08-20 21:39:34 +0000

    🐘🐯 Checkpoint ./public/multiplayer.js:64788585/413 ./public/index.html:64788585/159

--- public/multiplayer.js
+++ public/multiplayer.js
@@ -10,12 +10,18 @@ setInterval(sendUpdate, 60)
 socket.on("player update", (stuff) =>{
   if(stuff.id != playerId && !(stuff.id in players)) {
     let newPlayer = document.createElement("a-entity");
+    let newPlayerHitbox = document.createElement("a-cylinder")
      newPlayer.setAttribute("move", "jkw")
     newPlayer.setAttribute("gltf-model", "https://cdn.glitch.global/756a4aaf-b43f-4a95-998c-1c3ac912e721/player_idle2.glb?v=1724004739221")
     newPlayer.setAttribute("scale", "0.4 0.4 0.4")
     newPlayer.setAttribute("visible", "true")
+    newPlayerHitbox.setAttribute("static-body", "shape", "cylinder")
+    newPlayerHitbox.setAttribute("visible", "false")
     players[stuff.id] = newPlayer;
-    scene.appendChild(newPlayer);
+    newPlayerHitbox.setAttribute("position", "0 5.25 0")
+    newPlayerHitbox.setAttribute("height", "4")
+    newPlayer.appendChild(newPlayerHitbox);
+    scene.appendChild(newPlayer)
   } else if(stuff.id in players) {
     stuff.position.y-=0.5;
     players[stuff.id].setAttribute("position", stuff.position);
