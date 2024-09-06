b9b97425dc185aef292179ca3c8e88f63627db2b
Glitch (glitch-hello-website) <none>
2024-09-06 12:04:57 +0000

    🍇🎖 Checkpoint ./public/spawn.js:62936102/648

--- public/spawn.js
+++ public/spawn.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   let player = document.getElementById("player");
+ function reSpawn() { 
+   let player = document.getElementById("player");
   let x = (Math.random()*10);
   let z = (Math.random()*10);
     player.setAttribute("position", {x: x, y: 5, z: z})
    player.setAttribute("dynamic-body", "shape", "sphere")
+ }
+  reSpawn()
+  setInterval(()=>{
+    if(player.getAttribute("position".y < -10)) {
+      reSpawn()
+    }
+  }, 60)
 });
