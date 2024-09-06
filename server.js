f86cf597567f836ef5c3d6783647b9d7739f28bf
Glitch (glitch-hello-website) <none>
2024-09-06 21:43:28 +0000

    ✋🙈 Checkpoint ./server.js:64788585/511 ./package.json:64788585/959 ./chat.json:64788585/2 ./public/multiplayer.js:64788585/123

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // Setup basic express server
 var express = require("express");
+var fs = require("fs")
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
-
+let chat = {};
+try {
+  const data = fs.readFileSync('chat.json');
+  chat = JSON.parse(data) || {};
+} catch (error) {
+  console.log('No existing votes found, starting fresh.');
+}
 server.listen(port, function () {
   console.log("Server listening at port %d", port);
 });
@@ -15,10 +22,12 @@ app.use(express.static("public"));
 // Chatroom
 io.on("connection", function (socket) {
   socket.on("chat message", function (data) {
-    
+    console.log(data)
+    chat[data.time] = data.message
+    fs.writeFileSync('chat.json', JSON.stringify(chat, null, 2));
+    socket.broadcast.emit("chat message", chat)
   })
   socket.on("player update", function (data) {
-    console.log(data);
     // we tell the client to execute 'new message'
     socket.broadcast.emit("player update", data);
   });
