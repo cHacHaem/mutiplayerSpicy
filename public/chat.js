8a3d8bedae688e119f83292224159fa9f5ac9fad
Glitch (glitch-hello-website) <none>
2024-09-09 13:35:07 +0000

    🍁💡 Checkpoint ./public/style.css:62936102/19 ./public/chat.js:62936102/228

--- public/chat.js
+++ public/chat.js
@@ -1,9 +1,7 @@
 let chatInput = document.getElementById("chat-input")
 let chatContent = document.getElementById("chat-content")
 chatInput.addEventListener('keydown', function(event) {
-  if (event.code === 'Space' || event.key === ' ') {
     event.stopPropagation();  // Prevent the spacebar event from reaching the game
-  }
 });
 function sendMessage() {
  socket.emit("chat message", {message: chatInput.value, time: Date.now(), id: playerId}) 
@@ -13,5 +11,9 @@ socket.on("chat message", (message)=>{
     let newMesText = document.createElement("h3");
     let newMesPerson = document.createElement("h2");
     newMesText.innerHtml = message.message;
+    newMesText.class = "message"
     newMesPerson.innerHtml = message.id;
+    newMes.appendChild(newMesPerson);
+    newMes.appendChild(newMesText);
+    chatContent.appendChild(newMes)
   })
