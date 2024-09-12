93a9cd0a2ceb357e70811ac414a59bd936d26b7e
Glitch (glitch-hello-website) <none>
2024-09-12 22:33:16 +0000

    🏬🍍 Checkpoint ./public/tag/tag.js:64788585/480

--- public/tag/tag.js
+++ public/tag/tag.js
@@ -1,18 +1,25 @@
+/* global playerId socket player players */
 let whoIt = "";
 let itMarker = document.createElement("a-sphere").setAttribute("color", "red");
 // add tag cooldown
-console.log(playerId)
+console.log(playerId);
 player.addEventListener('collide', function (e) {
   let otherDude = e.detail.body.el.id;
   console.log(otherDude);
   if(otherDude == whoIt) {
-    socket.emit("player tagged", playerId)
-    whoIt = playerId
+    socket.emit("player tagged", playerId);
+  tagPlayer(playerId);
   } else if(otherDude in players) {
-    socket.emit("player tagged", otherDude)
-    whoIt = otherDude
+    socket.emit("player tagged", otherDude);
+    tagPlayer(otherDude);
   }
 });
 function tagPlayer(taggedPlayer) {
-  players[taggedPlayer].entity.appendChild()
+  if(playerId == taggedPlayer) {
+    player.appendChild(itMarker)
+  } else {
+    players[taggedPlayer].entity.appendChild(itMarker); 
+  }
+  whoIt = taggedPlayer;
+  console.log(players[taggedPlayer]);
 }
