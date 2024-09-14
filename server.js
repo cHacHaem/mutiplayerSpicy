ce1399bf1a54fb9c5bf4039dadeb8fff63124631
Glitch (glitch-hello-website) <none>
2024-09-14 14:12:49 +0000

    👳🎳 Checkpoint ./server.js:64788585/4552

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 // Setup basic express server
 var express = require("express");
-var fs = require("fs")
+var fs = require("fs");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 let chat = {};
-let game = {hub: {}, tag1: {started: false}, tag2: {started: false}, tag3: {started: false}}
+let game = {
+  hub: { players: [] },
+  tag1: {  players: [], started: false, whoIt: "undecided" },
+  tag2: {  players: [], started: false, whoIt: "undecided" },
+  tag3: {  players: [], started: false, whoIt: "undecided" },
+};
 try {
-  const data = fs.readFileSync('chat.json');
+  const data = fs.readFileSync("chat.json");
   chat = JSON.parse(data) || {};
 } catch (error) {
-  console.log('No existing votes found, starting fresh.');
+  console.log("No existing votes found, starting fresh.");
 }
 server.listen(port, function () {
   console.log("Server listening at port %d", port);
@@ -23,35 +28,40 @@ app.use(express.static("public"));
 // Chatroom
 io.on("connection", function (socket) {
   let world;
-  let whoIt;
   let playerId;
-  socket.on("world", (data)=>{
-    if(data.world == "hub") {
-     world = "hub"
+  socket.on("world", (data) => {
+    if (data.world == "hub") {
+      world = "hub";
       playerId = data.id;
-      socket.join(world)
-    } else if(data.world == "tag") {
-      if(game.tag1)
-      world = "tag1"
-      playerId = data.id
-      socket.join(world)
-      socket.on("player tagged", (data)=>{
-        console.log("player tagged: ", data)
-        socket.to(world).emit("player tagged", data)
-        whoIt = data
-      })
+      socket.join(world);
+    } else if (data.world == "tag") {
+      if (!game.tag1.started) {
+        world = "tag1";
+        game.tag1.players.push(playerId)
+      } else if (!game.tag2.started) {
+        world = "tag2";
+        game.tag2.players.push(playerId)
+      } else if (!game.tag3.started) {
+        world = "tag3";
+        game.tag3.players.push(playerId)
+      }
+      playerId = data.id;
+      socket.join(world);
+      socket.on("player tagged", (data) => {
+        console.log("player tagged: ", data);
+        socket.to(world).emit("player tagged", data);
+      });
     }
-      
-  })
+  });
   socket.on("chat message", function (data) {
-    console.log(data)
-    chat[data.time] = {message: data.message, id: data.id, name: data.name}
-   // fs.writeFileSync('chat.json', JSON.stringify(chat, null, 2));
-   // const data2 = fs.readFileSync('chat.json');
-  //chat = JSON.parse(data2) || {};
+    console.log(data);
+    chat[data.time] = { message: data.message, id: data.id, name: data.name };
+    // fs.writeFileSync('chat.json', JSON.stringify(chat, null, 2));
+    // const data2 = fs.readFileSync('chat.json');
+    //chat = JSON.parse(data2) || {};
     //console.log(chat)
-    socket.to(world).emit("chat message", data)
-  })
+    socket.to(world).emit("chat message", data);
+  });
   socket.on("player update", function (data) {
     // we tell the client to execute 'new message'
     socket.to(world).emit("player update", data);
