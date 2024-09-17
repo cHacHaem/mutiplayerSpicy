f46236b7f5a9a1245bb4fdc5e7ec447b7d96e9aa
Glitch (glitch-hello-website) <none>
2024-09-17 21:00:13 +0000

    🐫📷 Checkpoint ./server.js:64788585/184 ./public/tag/index.html:64788585/20

--- server.js
+++ server.js
@@ -73,6 +73,9 @@ io.on("connection", function (socket) {
   const randomIndex = Math.floor(Math.random() * players.length);
     game[world].whoIt = players[randomIndex]
     io.to(world).emit("game start", players[randomIndex])
+    setInterval(()=>{
+      socket.to(world).emit("player tagged", game.world.whoIt)
+    })
   }
   //general
   socket.on("chat message", function (data) {
@@ -94,7 +97,9 @@ io.on("connection", function (socket) {
     let index; 
     if(game[world].players) index = game[world].players.indexOf(playerId); // Find the index of the string
   if (index !== -1) { // Check if the string exists in the array
+    game[world].started = false
     game[world].players.splice(index, 1); // Remove the string at the found index
   }
+    console.log(game)
   });
 });
