35a9789f11a519d2f87913a1b56624015006602b
Glitch (glitch-hello-website) <none>
2024-10-03 15:10:34 +0000

    🍂👢 Checkpoint ./public/tag/index.html:64788585/18 ./public/spawn.js:64788585/157

--- public/spawn.js
+++ public/spawn.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let x = (Math.random()*10);
   let z = (Math.random()*10);
     player.setAttribute("position", {x: x, y: 20, z: z})
-   player.setAttribute("dynamic-body", "shape", "sphere")
+   player.setAttribute("dynamic-body", {shape: "sphere", linearDamping: 0.9, angularDamping: 0.9})
  }
   reSpawn()
   setInterval(()=>{
