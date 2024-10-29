65807840c086e5d46c7836d41767c4e1be25c184
Glitch (mmspicy) <none>
2024-10-29 14:00:41 +0000

    💕🌄 Checkpoint ./public/tag/map.js:62936102/1233 ./public/tag/index.html:62936102/35 ./public/tag/tag.js:62936102/46

--- public/tag/map.js
+++ public/tag/map.js
@@ -1,4 +1,13 @@
-let map = document.getElementById("map")
+document.addEventListener("DOMContentLoaded", (event) => {
+  let map = document.getElementById("map")
+console.log(map)
+console.warn("hiiii")
 for(let i = 0; i < 10; i++){
-  
+  let rock = document.createElement("a-entity")
+  rock.mixin = "rock"
+  rock.position = {x: Math.random()*50, y: 0, z: Math.random()*50}
+  rock.scale = {x: Math.random()*3, y: Math.random()*3, z: Math.random()*3}
+  map.appendChild(rock)
+  console.log(rock)
 }
+});
