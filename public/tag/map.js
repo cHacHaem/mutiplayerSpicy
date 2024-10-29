2c6dd14e7a3bb79848e8214e1836c8a9b4e01bfb
Glitch (mmspicy) <none>
2024-10-29 14:11:51 +0000

    🍲💵 Checkpoint ./public/tag/map.js:62936102/1825

--- public/tag/map.js
+++ public/tag/map.js
@@ -1,13 +1,20 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let map = document.getElementById("map")
+let done = false;
+document.addEventListener("click", (event) => {
+  if(!done) {
+    let map = document.getElementById("map")
 console.log(map)
 console.warn("hiiii")
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < 30; i++){
   let rock = document.createElement("a-entity")
-  rock.mixin = "rock"
-  rock.position = {x: Math.random()*50, y: 0, z: Math.random()*50}
-  rock.scale = {x: Math.random()*3, y: Math.random()*3, z: Math.random()*3}
+  rock.setAttribute("mixin", "rock")
+  rock.setAttribute("position", {x: (Math.random()*100)-50, y: 0, z: (Math.random()*100)-50})
+  let x, y, z = 0;
+  while(x < 1) x = Math.random()*3
+  while(y < 1) y = Math.random()*3
+  while(z < 1) z = Math.random()*3
+  rock.setAttribute("scale", {x: x, y: y, z})
   map.appendChild(rock)
   console.log(rock)
-}
+  done = true;
+}}
 });
