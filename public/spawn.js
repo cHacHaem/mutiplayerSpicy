842302d68a19b54c9c43b241aa11c41ff3c1f067
Glitch (mmspicy) <none>
2024-11-01 12:19:42 +0000

    🏀🎟 Checkpoint ./public/index.html:62936102/587 ./public/spawn.js:62936102/6

--- public/spawn.js
+++ public/spawn.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   let player = document.getElementById("player");
  function reSpawn() { 
    let player = document.getElementById("player");
-  let x = (Math.random()*10);
-  let z = (Math.random()*10);
+  let x = (Math.random()*5);
+  let z = (Math.random()*5);
     player.setAttribute("position", {x: x, y: 20, z: z})
    player.setAttribute("dynamic-body", {shape: "sphere", linearDamping: 0.9, angularDamping: 0.9})
  }
